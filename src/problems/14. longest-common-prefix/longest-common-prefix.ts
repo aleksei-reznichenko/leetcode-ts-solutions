e92bc3e555b53a525e7201141b9f11fb444b17a9
Aleksey REZNICHENKO <aleksey.reznichenko@raiffeisen.ru>
2025-02-27 23:21:45 +0200

    chore(problems): add jsdoc

--- src/problems/14. longest-common-prefix/longest-common-prefix.ts
+++ src/problems/14. longest-common-prefix/longest-common-prefix.ts
@@ -1,3 +1,12 @@
+/**
+ * Finds the longest common prefix of a given array of strings.
+ *
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ *
+ * @param strings the array of strings
+ * @returns the longest common prefix, or an empty string if no common prefix exists
+ */
 export function longestCommonPrefix(strings: string[]): string {
   let result = ''
 
