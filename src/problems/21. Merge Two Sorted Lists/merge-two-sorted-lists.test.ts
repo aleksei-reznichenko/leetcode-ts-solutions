a28fe4a44b21007e4597f89640356ae84b430c3e
Aleksey REZNICHENKO <aleksey.reznichenko@raiffeisen.ru>
2025-03-03 00:18:05 +0200

    chore: fix linter warning

--- src/problems/21. Merge Two Sorted Lists/merge-two-sorted-lists.test.ts
+++ src/problems/21. Merge Two Sorted Lists/merge-two-sorted-lists.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals'
-import { mergeTwoLists, ListNode } from './merge-two-sorted-lists'
+import { ListNode, mergeTwoLists } from './merge-two-sorted-lists'
 
 describe('mergeTwoLists', () => {
   test('empty lists', () => {
